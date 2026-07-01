c36a1996057c6ffd59e17a0152d40f03610d6fae
Stephen Bicker <stephen.bicker@gmail.com>
2026-06-30 22:56:25 -0700

    Initial Commit

---
+++ Pocket Cabbage/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Pocket Cabbage
+//
+//  Created by Stephen Bicker on 6/30/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Pocket CabbageTests/Pocket_CabbageTests.swift
@@ -0,0 +1,18 @@
+//
+//  Pocket_CabbageTests.swift
+//  Pocket CabbageTests
+//
+//  Created by Stephen Bicker on 6/30/26.
+//
+
+import Testing
+
+struct Pocket_CabbageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
